15ab7b33cd740dc776d1b954bb4f80591646630d
yanhuichao <liuyingji.me@gmail.com>
2017-01-07 17:45:24 +0800

    modify environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
 var webpack = require('webpack')
 var path = require('path')
 module.exports = {
-  entry: path.resolve('./entry.js'),
+  entry: [
+  	'webpack/hot/dev-server',
+  	'webpack-dev-server/client?http://localhost:8080', 
+  	path.resolve(__dirname, 'app/entry.js')
+  ],
   output: {
-    path: __dirname,
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js'
   },
   module: {
     loaders: [
       {test: /\.css$/, loader: 'style!css'}
     ]
+  },
+  devServer:{
   },
   plugins: [
     new webpack.BannerPlugin('This file is created by radio')
